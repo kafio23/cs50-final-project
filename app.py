fe52a73cc588895cb4bf24203f9f23f5d6bb3be4
Fiorella Quino O. <dkafio@gmail.com>
2024-07-18 20:54:48 -0700

    add spotipy and use recommendations

--- app.py
+++ app.py
@@ -1,41 +1,47 @@
+import os
+import spotipy
 from flask import Flask, render_template, jsonify, request
+from dotenv import load_dotenv
 from transformers import pipeline
+from spotipy.oauth2 import SpotifyClientCredentials
 
 app = Flask(__name__,  template_folder='template')
 
-emotion_pipeline = pipeline('text-classification', model='j-hartmann/emotion-english-distilroberta-base', return_all_scores=True)
+load_dotenv()
+client_id = os.getenv("client_id")
+client_secret = os.getenv("client_secret")
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+emotion_pipeline = pipeline('text-classification', model='j-hartmann/emotion-english-distilroberta-base', top_k=None)
 classifier = pipeline(task="sentiment-analysis")
 
-@app.route('/index')
-def hello():
-    text = "This was a great day, I would like to talk to everyone!"
-    emotions = emotion_pipeline(text)
-    preds = classifier(text)
-    print(emotions)
-    print(preds)
-    preds = [{"score": round(pred["score"], 4), "label": pred["label"]} for pred in preds]
-    print(preds)
-    
-    return render_template("home.html")
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
+@app.route('/', methods=['GET'])
+def home():    
+    return render_template("base.html")
+
+
+@app.route('/emotify', methods=['POST'])
+def emotify():
     if request.method == 'POST':
         user_text = request.form.get('textareaResponse', '')
         print(user_text)
 
-    emotions = emotion_pipeline(user_text)
-    preds = classifier(user_text)
-    print(emotions)
-    print(preds)
+        emotions = emotion_pipeline(user_text)
+        preds = classifier(user_text)
+        print(emotions)
+        print(preds)
     
-    return render_template("base.html")
+    print('Inside!!')
+    print(f'My id is: {client_id}.')
+    print(f'My secret key is: {client_secret}.')
+    genres = ['classical', 'jazz']
+    # TODO: CustomModel
+    recommendations = get_spotify_recommendations(genres)
+    print(f"Recommended genres: {recommendations['tracks'][0]}")
+    return jsonify({'data': recommendations['tracks'][0]})
 
-@app.route('/data', methods=['GET'])
-def get_data():
-    data = [
-        {'name': 'John Doe', 'age': 32, 'city': 'New York'},
-        {'name': 'Jane Doe', 'age': 27, 'city': 'London'},
-        {'name': 'Jim Smith', 'age': 45, 'city': 'Paris'},
-    ]
-    return jsonify({'data': data})
+def get_spotify_recommendations(genres):
+    recommendations = sp.recommendations(seed_genres=genres, limit=10)
+    return recommendations
--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,85 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const App = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({});
+  const [isButtonDisabled, setButtonDisabled] = useState(false);
+  const [textareaResponse, setTextareaResponse] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const userInput = { textareaResponse: textareaResponse };
 
-  useEffect(() => {
-    axios.get('http://127.0.0.1:5000/data')
+  const handleTextareaChange = (event) => {
+    setTextareaResponse(event.target.value);
+  };
+
+  const enableButton = () => {
+    setButtonDisabled(true);
+  };
+
+  const updateImageUrl = (data) => {
+    setImageUrl(data.album.images[0].url)
+  }
+
+  const submit = () => {
+    enableButton();
+
+    axios.post('http://127.0.0.1:5000/emotify', userInput)
       .then(res => {
         setData(res.data.data);
+        updateImageUrl(res.data.data);
       })
-  }, []);
+  };
 
   return (
     <div className='container text-center'>
+      <script src="https://sdk.scdn.co/spotify-player.js"></script>
+
       <div className='mt-4 mb-2'>
         <br></br>
         <h1>Welcome to Emotify!</h1>
       </div>
       <div className='form-container mt-4'>
+        <div className='spotify-div' hidden={!imageUrl}>
+          <img src={imageUrl} alt='Logo' />
+        </div>
         <div className='form-div'>
-          <form className='row mt-3' action='http://127.0.0.1:5000/' method='post'>
-            <div className='mb-3'>
+          <form className='row mt-3'>
+            <div className='spinner-div mt-3 mb-4' hidden={!isButtonDisabled}>
+              <div className='spinner-grow' style={{ marginRight: '7px' }} role='status'>
+                <span className='visually-hidden'>Loading...</span>
+              </div>
+              <div className='spinner-grow' role='status'>
+                <span className='visually-hidden'>Loading...</span>
+              </div>
+              <div className='spinner-grow' style={{ marginLeft: '7px' }} role='status'>
+                <span className='visually-hidden'>Loading...</span>
+              </div>
+            </div>
+            <div className='mb-3' hidden={isButtonDisabled}>
               <label className='form-label main-label'>Share how you feel today!</label>
               <div className='textarea-div'>
-                <textarea className='form-control mt-2' id='textareaResponseId' name='textareaResponse' rows='3' required='required'></textarea>
+                <textarea
+                  className='form-control mt-2'
+                  id='textareaResponseId'
+                  name='textareaResponse'
+                  rows='3'
+                  required='required'
+                  value={textareaResponse}
+                  onChange={handleTextareaChange}
+                ></textarea>
               </div>
             </div>
             <div className='col'>
-              <button type="submit" className='btn btn-lg mb-3'>Submit</button>
+              <button
+                onClick={submit}
+                className='btn btn-lg mb-3'
+                disabled={isButtonDisabled}
+                hidden={isButtonDisabled}
+              >Submit</button>
             </div>
           </form>
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
